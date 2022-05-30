28ff3cc5cfd85f4771ea76ef95683e6f946080a4
Dave Feucht <dfeucht@lequest.com>
2022-05-30 09:16:11 +0200

    Update styling of Axis component

--- src/Components/Axis.jsx
+++ src/Components/Axis.jsx
@@ -1,35 +1,24 @@
 import PropTypes from 'prop-types';
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import AxisItem from './AxisItem';
 import './Axis.css';
 
 const Axis = ({ direction = 'row' }) => {
-    const [axisLabels, setAxisLabels] = useState([]);
-    const axisRef = useRef(null);
-
-    useEffect(() => {
-        if (axisRef.current) {
-            axisRef.current.style.setProperty('--axis-direction', direction);
-
-            if (direction === 'row') {
-                axisRef.current.style.setProperty('--axis-width', '500px');
-                axisRef.current.style.setProperty('--axis-height', '20px');
-                axisRef.current.style.setProperty('--axis-left', '30px');
-                axisRef.current.style.setProperty('--axis-top', '0px');
-                setAxisLabels(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']);
-            } else {
-                axisRef.current.style.setProperty('--axis-width', '20px');
-                axisRef.current.style.setProperty('--axis-height', '500px');
-                axisRef.current.style.setProperty('--axis-left', '0px');
-                axisRef.current.style.setProperty('--axis-top', '30px');
-                setAxisLabels(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']);
-            }
+    const getAxisLabels = direction => {
+        switch (direction) {
+            case 'row':
+                return ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+                break;
+            case 'column':
+                return ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+                break;
+            default:
         }
-    }, []);
+    }
 
     return (
-        <div ref={axisRef} className="battleship__axis">
-            {axisLabels.map((label, index) => {
+        <div className={`battleship__axis ${direction}`}>
+            {getAxisLabels(direction).map((label, index) => {
                 return <AxisItem key={`axis_label_${label}`} label={label} />
             })}
         </div>
